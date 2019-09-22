5583a3760e0daa6cbbbfe883a5567f8ff6846e56
Marc Neudert <marc.neudert@gmail.com>
2019-09-22 13:54:12 +0200

    Allow reading all tree pointers from a database

--- lib/mmdb2_hacks.ex
+++ lib/mmdb2_hacks.ex
@@ -1,6 +1,8 @@
 defmodule MMDB2Hacks do
   @moduledoc false
 
+  use Bitwise, only_operators: true
+
   @decode_options [double_precision: nil, float_precision: nil, map_keys: :strings]
 
   @doc """
@@ -28,5 +30,53 @@ defmodule MMDB2Hacks do
           Map.has_key?(&1, "names"))
     )
     |> Enum.uniq_by(&Map.get(&1, "geoname_id"))
+  end
+
+  @doc """
+  Extract all pointers referenced in the lookup tree.
+  """
+  @spec tree_pointers(Path.t()) :: [non_neg_integer]
+  def tree_pointers(database_path) do
+    {:ok, meta, tree, _data} =
+      database_path
+      |> File.read!()
+      |> MMDB2Decoder.parse_database()
+
+    tree
+    |> tree_pointers_extract(meta, [])
+    |> Enum.uniq()
+    |> Enum.map(&(&1 - meta.node_count - 16))
+  end
+
+  defp tree_pointers_extract("", _, pointers), do: pointers
+
+  defp tree_pointers_extract(
+         tree,
+         %{node_count: node_count, record_size: record_size} = meta,
+         pointers
+       ) do
+    record_half = rem(record_size, 8)
+    record_left = record_size - record_half
+
+    <<left_low::size(record_left), left_high::size(record_half), pointer_right::size(record_size),
+      rest::binary>> = tree
+
+    pointer_left = left_low + (left_high <<< record_left)
+
+    pointers =
+      if pointer_left > node_count do
+        [pointer_left | pointers]
+      else
+        pointers
+      end
+
+    pointers =
+      if pointer_right > node_count do
+        [pointer_right | pointers]
+      else
+        pointers
+      end
+
+    tree_pointers_extract(rest, meta, pointers)
   end
 end
